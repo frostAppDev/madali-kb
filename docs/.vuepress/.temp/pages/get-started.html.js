f8006d2492780a06e83c17db0218a0a2728e2f96
Mike Navales <johnmichael.navales@gmail.com>
2025-04-05 04:50:52 +0800

    Initial deploy

--- docs/.vuepress/.temp/pages/get-started.html.js
+++ docs/.vuepress/.temp/pages/get-started.html.js
@@ -1,16 +1,3 @@
 import comp from "C:/Users/Mike Navales/DEV/VSCode Files/frostAppDev/madali-kb/docs/.vuepress/.temp/pages/get-started.html.vue"
 const data = JSON.parse("{\"path\":\"/get-started.html\",\"title\":\"Get Started\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Pages\",\"slug\":\"pages\",\"link\":\"#pages\",\"children\":[]},{\"level\":2,\"title\":\"Content\",\"slug\":\"content\",\"link\":\"#content\",\"children\":[]},{\"level\":2,\"title\":\"Configuration\",\"slug\":\"configuration\",\"link\":\"#configuration\",\"children\":[]},{\"level\":2,\"title\":\"Layouts and customization\",\"slug\":\"layouts-and-customization\",\"link\":\"#layouts-and-customization\",\"children\":[]}],\"git\":{\"updatedTime\":1743792551000,\"contributors\":[{\"name\":\"Mike Navales\",\"username\":\"\",\"email\":\"johnmichael.navales@gmail.com\",\"commits\":1}],\"changelog\":[{\"hash\":\"6c020993677fa1b78fa97b6f77fdadede331cc64\",\"time\":1743792551000,\"email\":\"johnmichael.navales@gmail.com\",\"author\":\"Mike Navales\",\"message\":\"Initial commit of existing project\"}]},\"filePathRelative\":\"get-started.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
